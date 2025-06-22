0ab2e07010752c190c1b56195941a76ce8adbe92
Carmel1215 <kth12153@gmail.com>
2025-06-22 21:21:03 +0900

    Feat: Player 코드 추가

---
+++ objects/player.py
@@ -0,0 +1,23 @@
+import pygame
+from core.loader import load
+
+class Player:
+    def __init__(self, x, y, speed=100):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.player = load('assets/images/Player.png')
+
+    def update(self, delta_time, keys):
+        dx = dy = 0
+
+        if keys[pygame.K_w]: dy -= 1
+        if keys[pygame.K_s]: dy += 1
+        if keys[pygame.K_a]: dx -= 1
+        if keys[pygame.K_d]: dx += 1
+
+        self.x += dx * self.speed * delta_time
+        self.y += dy * self.speed * delta_time
+
+    def draw(self, screen):
+        screen.blit(self.player, (self.x, self.y))
