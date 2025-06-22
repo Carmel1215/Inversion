e2d97f3b520200734e16a83cbf2b32099982ce7f
Carmel1215 <kth12153@gmail.com>
2025-06-22 15:01:58 +0900

    Del: 예시 코드 삭제

--- scenes/intro_scene.py
+++ scenes/intro_scene.py
@@ -5,13 +5,6 @@ class IntroScene(Scene):
     def __init__(self, game):
         super().__init__(game)
         # 필요한 것 초기화
-        self.x = SCREEN_WIDTH / 2
-        self.y = 700
-        self.to_x = 50
-        self.to_y = 50
-        self.speed = 10
-        self.arrived = False
-        self.image = pygame.image.load('assets/images/IntroScene.png')
 
 
     def handle_events(self, events):
@@ -21,19 +14,7 @@ class IntroScene(Scene):
                     print("Space Pressed")
 
     def update(self):
-        if not self.arrived:
-            self.y -= self.speed
-            if self.y <= self.to_y:
-                self.y = self.to_y
-                self.arrived = True
-
-        if not self.arrived:
-            self.x -= self.speed
-            if self.x <= self.to_x:
-                self.x = self.to_x
-                self.arrived = True
-
+        pass
 
     def draw(self, screen):
-        screen.fill((255, 255, 255))
-        screen.blit(self.image, (self.x, self.y))
+        pass
