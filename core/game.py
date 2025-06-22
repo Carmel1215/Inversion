7b330c8b03706a89cc65a389367da7958eb7b298
Carmel1215 <kth12153@gmail.com>
2025-06-22 21:19:07 +0900

    Chore: 디버깅용 FPS 표시 추가 및 전체화면 모드로 변경

--- core/game.py
+++ core/game.py
@@ -1,11 +1,12 @@
 import pygame
 import core.settings as settings
+from core.settings import *
 from core.scene_manager import SceneManager
 from scenes.intro_scene import IntroScene
 
 class Game:
     def __init__(self):
-        self.screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         pygame.display.set_caption(settings.TITLE)
         # pygame.display.set_icon(settings.icon)
         self.clock = pygame.time.Clock()
@@ -16,15 +17,20 @@ class Game:
 
     def run(self):
         '''메인 루프'''
-        while self.running:
-            delta_time = self.clock.tick(settings.FPS) / 1000 # deltatime
 
+        font = pygame.font.SysFont("Arial", 16)
+        while self.running:
+            fps = self.clock.get_fps()
+            delta_time = self.clock.tick(settings.FPS) / 1000 # deltatime
             events = pygame.event.get()
             self.handle_quit(events)
             self.scene_manager.handle_events(events)
             self.scene_manager.update(delta_time)
             self.scene_manager.draw(self.screen)
-            pygame.display.flip()
+            fps_text = font.render(f"FPS: {fps:.1f}", True, (0, 255, 0))
+            self.screen.blit(fps_text, (10, 10))
+
+            pygame.display.update()
 
     def handle_quit(self, events):
         '''모든 씬에 적용되는 프로그램 종료 함수'''
