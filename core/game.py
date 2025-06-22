db4b9a69a9e22a81330a5292f6a23481e06dbf9d
Carmel1215 <kth12153@gmail.com>
2025-06-22 14:57:45 +0900

    Feat: intro_scene 예시 코드 작성 및 종료 함수 이름 변경

--- core/game.py
+++ core/game.py
@@ -18,14 +18,14 @@ class Game:
         '''메인 루프'''
         while self.running:
             events = pygame.event.get()
-            self.handle_events(events)
+            self.handle_quit(events)
             self.scene_manager.handle_events(events)
             self.scene_manager.update()
             self.scene_manager.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(settings.FPS)
 
-    def handle_events(self, events):
+    def handle_quit(self, events):
         '''모든 씬에 적용되는 프로그램 종료 함수'''
         for event in events:
             if event.type == pygame.QUIT:
--- scenes/intro_scene.py
+++ scenes/intro_scene.py
@@ -1,10 +1,18 @@
 from scenes.scene import Scene
-import pygame
+from core.settings import *
 
 class IntroScene(Scene):
     def __init__(self, game):
         super().__init__(game)
         # 필요한 것 초기화
+        self.x = SCREEN_WIDTH / 2
+        self.y = 700
+        self.to_x = 50
+        self.to_y = 50
+        self.speed = 10
+        self.arrived = False
+        self.image = pygame.image.load('assets/images/IntroScene.png')
+
 
     def handle_events(self, events):
         for event in events:
@@ -13,7 +21,19 @@ class IntroScene(Scene):
                     print("Space Pressed")
 
     def update(self):
-        pass
+        if not self.arrived:
+            self.y -= self.speed
+            if self.y <= self.to_y:
+                self.y = self.to_y
+                self.arrived = True
+
+        if not self.arrived:
+            self.x -= self.speed
+            if self.x <= self.to_x:
+                self.x = self.to_x
+                self.arrived = True
+
 
     def draw(self, screen):
-        screen.fill((0, 0, 0))
+        screen.fill((255, 255, 255))
+        screen.blit(self.image, (self.x, self.y))
