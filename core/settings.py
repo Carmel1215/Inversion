c8e5a5824c6890856b481e0cc9e4de32163ece5b
Carmel1215 <kth12153@gmail.com>
2025-07-04 22:43:12 +0900

    Chore: 하얀색과 검은색 눈이 편안하게 수정

--- core/settings.py
+++ core/settings.py
@@ -5,5 +5,5 @@ SCALE = 4
 # icon = pygame.image.load("assets/images/icon.png")
 
 # 색깔
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+WHITE = (250, 250, 250) # Hex 값: FAFAFA
+BLACK = (18, 18, 18) # Hex 값: 121212
