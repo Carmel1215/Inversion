c56cdfadc66fd63abf751b48de256f71275bc003
Carmel1215 <kth12153@gmail.com>
2025-07-04 22:41:38 +0900

    Feat: loader.py에 이미지 캐싱 기능 추가

--- core/loader.py
+++ core/loader.py
@@ -1,8 +1,16 @@
 import pygame
 from core.settings import SCALE
 
+images = {} # 이미지 모아두는 딕셔너리
+
 def load(path : str, scale : float = SCALE) -> pygame.Surface:
+    key = (path, scale)
+
+    if key in images:
+        return images[key]
     image = pygame.image.load(path).convert_alpha()
     width, height = image.get_size()
     scaled_image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+
+    images[key] = scaled_image
     return scaled_image
