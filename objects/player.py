a38aa5bd710d46bc02bc38475b4c30a6cb63a6c7
Carmel1215 <kth12153@gmail.com>
2025-07-04 22:45:21 +0900

    Chore: 이미지 변경 TODO 추가

--- objects/player.py
+++ objects/player.py
@@ -2,7 +2,7 @@ import pygame, os
 from core.loader import load
 
 class Player:
-    def __init__(self, x, y, speed=100):
+    def __init__(self, x, y, speed=200):
         self.x = x
         self.y = y
         self.speed = speed
@@ -11,10 +11,18 @@ class Player:
     def update(self, delta_time, keys):
         dx = dy = 0
 
-        if keys[pygame.K_w]: dy -= 1
-        if keys[pygame.K_s]: dy += 1
-        if keys[pygame.K_a]: dx -= 1
-        if keys[pygame.K_d]: dx += 1
+        if keys[pygame.K_w]:
+            dy -= 1
+            # TODO: 다른 방향 바라보는 이미지로 변경 코드 작성
+        if keys[pygame.K_s]:
+            dy += 1
+            # TODO: 다른 방향 바라보는 이미지로 변경 코드 작성
+        if keys[pygame.K_a]:
+            dx -= 1
+            # TODO: 다른 방향 바라보는 이미지로 변경 코드 작성
+        if keys[pygame.K_d]:
+            dx += 1
+            # TODO: 다른 방향 바라보는 이미지로 변경 코드 작성
 
         self.x += dx * self.speed * delta_time
         self.y += dy * self.speed * delta_time
