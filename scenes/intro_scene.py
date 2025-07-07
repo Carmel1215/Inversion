ddc56ab55d12b74def73c4a857b271847d0378f2
Carmel1215 <kth12153@gmail.com>
2025-07-07 17:19:03 +0900

    Feat: 타이틀 추가

--- scenes/intro_scene.py
+++ scenes/intro_scene.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, os
 from objects.player import Player
 from scenes.scene import Scene
 from core.settings import *
@@ -7,6 +7,10 @@ class IntroScene(Scene):
     def __init__(self, game):
         super().__init__(game)
         self.player = Player(400, 500)
+
+        self.font = pygame.font.Font(FONT_PATH, 256)
+
+        self.title_text = self.font.render('Inversion', True, (255, 255, 255))
 
     def handle_events(self, events):
         for event in events:
@@ -17,5 +21,13 @@ class IntroScene(Scene):
         self.player.update(delta_time, keys)
 
     def draw(self, screen):
+        text_rect = self.title_text.get_rect(centerx=screen.get_width() / 2, centery=screen.get_height() / 6)
+
+        # BackGround
         screen.fill(BLACK)
+
+        # UI
+        screen.blit(self.title_text, text_rect)
+
+        # Player
         self.player.draw(screen)
