ecd67e5698c3e879643b4967a0caedd1ba6f24a1
Carmel1215 <kth12153@gmail.com>
2025-07-08 19:53:23 +0900

    Refactor: button.py 위치 수정

--- objects/button.py
+++
@@ -1,5 +0,0 @@
-import pygame
-
-
-class Button(pygame.sprite.Sprite):
-    pass
---
+++ ui/button.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, game):
+        pass
+
+    def draw(self, screen):
+        pass
